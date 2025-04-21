957f89b8bf41e7286c4c3e955206a7448fd4e858
Leonel Salgueiro <leonel.ifcs@outlook.com>
2025-04-21 11:37:21 -0300

    without loop

--- conversor.py
+++ conversor.py
@@ -3,33 +3,27 @@ dolar = 5.80
 euro = 6.60
 yuan = 1.26
 
-while True:
- print("Conversor de Moedas em Reais")
- print("Moedas Disponíveis")
- print("1. Dólar")
- print("2. Euro")
- print("3. Yuan")
- print("4. Sair")
+print("Conversor de Moedas em Reais")
+print("Moedas Disponíveis")
+print("1. Dólar")
+print("2. Euro")
+print("3. Yuan")
 
- value_str = input("Escolha o valor para conversão em reais (R$): ")
- value = float(value_str.replace(",", "."))
- option = input("Escolha uma opção para conversão: ")
- 
+value_str = input("Escolha o valor para conversão em reais (R$): ")
+value = float(value_str.replace(",", "."))
+option = input("Escolha uma opção para conversão: ")
 
- if option == "1":
+if option == "1":
   converter = value / dolar
   print(f"R$ {value:.2f} = US$ {converter:.2f}")
 
- elif option == "2":
+elif option == "2":
   converter = value / euro
   print(f"R$ {value:.2f} = US$ {converter:.2f}")
 
- elif option == "3":
+elif option == "3":
   converter = value / yuan
   print(f"R$ {value:.2f} = US$ {converter:.2f}")
-
- elif option == "4":
-  break
  
- else:
+else:
   print("Opção inválida, digite uma opção correta")
