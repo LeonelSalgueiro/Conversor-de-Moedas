44d7fa6a0c436f86f064ae5adf5c6499a70d62bf
Leonel Salgueiro <leonel.ifcs@outlook.com>
2025-04-21 10:41:36 -0300

    init options

---
+++ conversor.py
@@ -0,0 +1,12 @@
+while True:
+ print("Conversor de Moedas em Reais")
+ print("Moedas Disponíveis")
+ print("1. Dólar")
+ print("2. Euro")
+ print("3. Yuan")
+ print("4. Sair")
+
+ escolha = input("Escolha uma opção para conversão: ")
+
+ if escolha == "4":
+  break
