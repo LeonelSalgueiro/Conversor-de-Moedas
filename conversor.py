c909c27945883187cf06b0fcac2dff058096d2d8
Leonel Salgueiro <leonel.ifcs@outlook.com>
2025-04-21 11:11:46 -0300

    format values

--- conversor.py
+++ conversor.py
@@ -1,3 +1,8 @@
+
+dolar = 5.80
+euro = 6.60
+yuan = 1.26
+
 while True:
  print("Conversor de Moedas em Reais")
  print("Moedas Disponíveis")
@@ -6,7 +11,24 @@ while True:
  print("3. Yuan")
  print("4. Sair")
 
+ valor = float(input("Escolha o valor para conversão em reais (R$): "))
  escolha = input("Escolha uma opção para conversão: ")
+ 
 
- if escolha == "4":
+ if escolha == "1":
+  conversor = valor / dolar
+  print(f"R$ {valor:.2f} = US$ {conversor:.2f}")
+
+ elif escolha == "2":
+  conversor = valor / euro
+  print(f"R$ {valor:.2f} = US$ {conversor:.2f}")
+
+ elif escolha == "3":
+  conversor = valor / yuan
+  print(f"R$ {valor:.2f} = US$ {conversor:.2f}")
+
+ elif escolha == "4":
   break
+ 
+ else:
+  print("Opção inválida, digite uma opção correta")
